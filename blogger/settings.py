af3575acde33cba6375b950e5618ae80f2d68b44
technologysansar <84139115+technologysansar@users.noreply.github.com>
2021-05-18 16:02:32 +0545

    Add files via upload

--- blogger/settings.py
+++ blogger/settings.py
@@ -166,6 +166,6 @@ MEDIA_URL = '/media/'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mishradevendra597@gmail.com'
-EMAIL_HOST_PASSWORD = "Damak@5632"
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ""
 
